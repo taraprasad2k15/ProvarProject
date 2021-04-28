98b385053afd2c6bca338e9cc26e010c02e62ba0
Taraprasad jena <taraprasad2k15@gmail.com>
2021-04-28 18:10:32 +0530

    adding files

---
+++ src/pageobjects/session0009/MyPageObject.java
@@ -0,0 +1,27 @@
+package pageobjects.session0009;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.provar.core.testapi.annotations.*;
+
+@Page( title="MyPageObject"                                
+     , summary=""
+     , relativeUrl=""
+     , connection="Regmain"
+     )             
+public class MyPageObject {
+
+	@TextType()
+	@FindBy(xpath = "//input[@id='input-408']")
+	public WebElement end;
+	@LinkType()
+	@FindBy(xpath = "//a[normalize-space(.)='AutomationContactPress Delete to Remove']")
+	public WebElement automationContact;
+	@TextType()
+	@FindBy(xpath = "//label[normalize-space(.)='Related To']/following-sibling::div//input")
+	public WebElement relatedTo;
+			
+}
